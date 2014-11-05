ce3226210f35cc84dc0f7e068bf0c8ad5e2e6fd1
Guangzhan <fuzhan317@gmail.com>
2014-11-05 20:35:36 +0800

    Create plusOne.cc

---
+++ plusOne.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        //if()
+        for(int i=digits.size()-1; i>=0; i--) {
+            digits[i]++;
+            if(digits[i] < 10) {
+                break;
+            }
+            digits[i]=0;
+        }
+        if(digits[0] == 0) {
+            digits[0] = 1;
+            digits.push_back(0);
+        }
+        return digits;
+    }
+};
